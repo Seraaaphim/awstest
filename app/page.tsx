36b6522366f24d2ed77bb1f62cb02f0971b3eb97
Seraaaphim <khryxbergolafermat@gmail.com>
2024-05-27 12:05:01 +0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+        <h1>Hello world</h1>
+      </div>
+    </main>
+  );
+}
