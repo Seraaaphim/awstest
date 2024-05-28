e42edd748c74ed11fd3b810fe137a9026d6459bc
Seraaaphim <khryxbergolafermat@gmail.com>
2024-05-28 14:43:19 +0800

    Add hi world

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <h1>Hello world</h1>
+        <h2>Hi world</h2>
       </div>
     </main>
   );
